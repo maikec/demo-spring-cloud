ef2ad556cc74a9d068146e4eb5562ecea9e2d418
maikec@126.com <a6259272>
2019-11-04 22:55:58 +0800

    初始化项目

---
+++ src/main/java/com/example/demospringcloud/DemoSpringCloudApplication.java
@@ -0,0 +1,13 @@
+package com.example.demospringcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringCloudApplication.class, args);
+    }
+
+}
